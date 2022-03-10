8838968d94e8d04556a4c40b0275a5d0f531006f
Nicolas Hamelin <n.hamelin44120@gmail.com>
2022-03-10 16:33:18 +0100

    correctif penalty box

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -98,9 +98,15 @@ public class Game {
 		System.out.println("They have rolled a " + roll);
 		
 		if (inPenaltyBox[currentPlayer]) {
-			int modulo = 2 + players.get(currentPlayer).nbTurnInJail;
-			System.out.println("you have 1 chance on " + modulo + " to get out of the penalty box");
-			if (roll % modulo != 0) {
+			Random rand = new Random();
+
+			System.out.println("nb turn : " + players.get(currentPlayer).nbTurnInJail);
+
+			int rollPenaltyBox = rand.nextInt(players.get(currentPlayer).nbTurnInJail) + 1;
+
+			System.out.println("roll : " + rollPenaltyBox);
+
+			if (rollPenaltyBox == 1) {
 				isGettingOutOfPenaltyBox = true;
 
 				System.out.println(players.get(currentPlayer).getName() + " is getting out of the penalty box");
@@ -123,7 +129,7 @@ public class Game {
 		
 			places[currentPlayer] = places[currentPlayer] + roll;
 			if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
-			
+
 			System.out.println(players.get(currentPlayer).getName()
 					+ "'s new location is " 
 					+ places[currentPlayer]);
@@ -200,6 +206,9 @@ public class Game {
 	public boolean wrongAnswer(){
 		System.out.println("Question was incorrectly answered");
 		System.out.println(players.get(currentPlayer).getName()+ " was sent to the penalty box");
+
+		players.get(currentPlayer).getNbTurnInJail();
+
 		inPenaltyBox[currentPlayer] = true;
 		this.gold = 0;
 
