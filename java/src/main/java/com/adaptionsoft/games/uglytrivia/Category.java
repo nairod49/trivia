39b38b787d58280f1301cd959a8950f115327853
Nicolas Hamelin <n.hamelin44120@gmail.com>
2022-03-10 10:44:59 +0100

    Class question & class category

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/Category.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Category.java
@@ -1,4 +1,29 @@
 package com.adaptionsoft.games.uglytrivia;
 
+import java.lang.reflect.Array;
+
 public class Category {
+    int idCategory;
+    String typeCategory;
+
+    public Category(int idCategory, String typeCategory) {
+        this.idCategory = idCategory;
+        this.typeCategory = typeCategory;
+    }
+
+    public int getIdCategory() {
+        return idCategory;
+    }
+
+    public void setIdCategory(int idCategory) {
+        this.idCategory = idCategory;
+    }
+
+    public String getTypeCategory() {
+        return typeCategory;
+    }
+
+    public void setTypeCategory(String typeCategory) {
+        this.typeCategory = typeCategory;
+    }
 }
--- java/src/main/java/com/adaptionsoft/games/uglytrivia/Question.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Question.java
@@ -1,4 +1,29 @@
 package com.adaptionsoft.games.uglytrivia;
 
+import java.util.LinkedList;
+
 public class Question {
+    int idQuestion;
+    Category questionCategory;
+
+    public Question(int idQuestion, Category questionCategory) {
+        this.idQuestion = idQuestion;
+        this.questionCategory = questionCategory;
+    }
+
+    public int getIdQuestion() {
+        return idQuestion;
+    }
+
+    public void setIdQuestion(int idQuestion) {
+        this.idQuestion = idQuestion;
+    }
+
+    public Category getQuestionCategory() {
+        return questionCategory;
+    }
+
+    public void setQuestionCategory(Category questionCategory) {
+        this.questionCategory = questionCategory;
+    }
 }
