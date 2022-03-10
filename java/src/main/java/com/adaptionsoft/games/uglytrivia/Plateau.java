f6bf17525b83c22168d0b93bccd5274074ff1288
Guillaume Gouy <guillaumegouy@sfr.fr>
2022-03-10 10:46:47 +0100

    classe Plateau

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/Plateau.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Plateau.java
@@ -1,4 +1,22 @@
 package com.adaptionsoft.games.uglytrivia;
 
+import java.util.ArrayList;
+
+
 public class Plateau {
+
+    ArrayList<Player>  listOfPlayer;
+    ArrayList<Integer> listOfCurrentPlayerPlace = new ArrayList();
+
+
+    public Plateau(ArrayList<Integer> listOfCruentPlayerPlace , ArrayList<Player> listOfPlayer ) {
+        this.listOfCurrentPlayerPlace = listOfCruentPlayerPlace;
+        for (int i= 0 ; i < this.listOfCurrentPlayerPlace.size() ; i ++){
+
+            this.listOfCurrentPlayerPlace.set(i, 0);
+        }
+
+
+    }
+
 }
